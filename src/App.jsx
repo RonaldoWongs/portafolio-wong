09e034d4baf7a8684243fb54e8cbd0583a982684
RonaldoWongs <deigamerwong@gmail.com>
2023-07-26 15:21:33 -0600

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,37 @@ import React from 'react';
 import Navbar from './components/NavbarComponent';
 import HeroComponent from './components/HeroComponent';
 import PortafolioComponent from './components/PortafolioComponent';
-import SkillBar from './components/SkillBar'
+import SkillBar from './components/SkillBar';
 import ContactComponent from './components/ContactComponent';
 import FooterComponent from './components/FooterComponent';
-
-
+import LoaderComponent from './components/LoaderComponent'; // Importa el componente LoaderComponent
 
 function App() {
+  // Simula un tiempo de carga antes de mostrar el contenido
+  // En tu caso, esto debería ser reemplazado por el contenido real
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    // Simula una carga de 3 segundos antes de mostrar el contenido
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <div>
-      <Navbar />
-      <HeroComponent />
-      <PortafolioComponent />
-      <SkillBar />
-      <ContactComponent />
-      <FooterComponent />
-     
-
+      {isLoading ? (
+        <LoaderComponent />
+      ) : (
+        <>
+          <Navbar />
+          <HeroComponent />
+          <PortafolioComponent />
+          <SkillBar />
+          <ContactComponent />
+          <FooterComponent />
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/LoaderComponent.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const LoaderComponent = () => {
+  return (
+    <div className="loader-container">
+      <div className="pyramid-loader">
+        <div className="wrapper">
+          <span className="side side1"></span>
+          <span className="side side2"></span>
+          <span className="side side3"></span>
+          <span className="side side4"></span>
+          <span className="shadow"></span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoaderComponent;
