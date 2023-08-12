8922e7202ebe87eb12a6046d13746891fe1478b7
RonaldoWongs <deigamerwong@gmail.com>
2023-08-12 02:46:58 -0600

    fondos

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import line from '../../video/line.mp4';
+import red from '../../video/red.mp4';
 
 function VideoBackground() {
   return (
     <video autoPlay loop muted className="video-background">
-      <source src={line} type="video/mp4" />
+      <source src={red} type="video/mp4" />
       {/* Add additional video sources for browsers that do not support MP4 format */}
     </video>
   );
